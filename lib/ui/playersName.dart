87ab116c8058014d870ae28a5892699e8b6a7dd7
ariaProgrammerFlutter <87ariyaahady@gmail.com>
2024-03-08 19:25:40 +0330

    tic_tac_toe

---
+++ lib/ui/playersName.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SingInPage extends StatefulWidget {
+  const SingInPage({super.key});
+
+  @override
+  State<SingInPage> createState() => _SingInPageState();
+}
+
+class _SingInPageState extends State<SingInPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.cyanAccent,
+      
+    );
+  }
+}
