d99a42ef080d6be6efc24a502a62234004eedcf8
ariaProgrammerFlutter <87ariyaahady@gmail.com>
2024-03-12 18:17:51 +0330

    adding fontas and images

--- lib/ui/playersName.dart
+++ lib/ui/playersName.dart
@@ -10,9 +10,36 @@ class SingInPage extends StatefulWidget {
 class _SingInPageState extends State<SingInPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.cyanAccent,
-      
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'نام بازیکن اول',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 30),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'نام بازیکن دوم',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 100),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('برو به بازی'),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
